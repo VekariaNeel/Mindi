ea5bfc6a76446de5409e82658ae47e30b1db8543
Neel Vekaria <vekariyaneelpvt@gmail.com>
2026-04-09 21:56:23 +0530

    Prepared project for production: Configured environments and secured CORS

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoZUC_LIktZsGqvjsmBdxUkSITYYphxUI",
-  authDomain: "mindi-d0bb7.firebaseapp.com",
-  projectId: "mindi-d0bb7",
-  storageBucket: "mindi-d0bb7.firebasestorage.app",
-  messagingSenderId: "467696955073",
-  appId: "1:467696955073:web:857cd9e7df97db14972e40",
-  measurementId: "G-XD115P9B14"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
